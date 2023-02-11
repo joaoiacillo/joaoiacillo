433cfe9fed2fcb0d529b73a0ee0a3fb5fb803a95
João Pedro <joaopiacillo@outlook.com.br>
2023-02-11 15:26:35 -0300

    Initial React Commit

---
+++ src/components/EmailMe.js
@@ -0,0 +1,9 @@
+import './EmailMe.css';
+
+export default function EmailMe() {
+    return (
+        <a href="mailto:joaopiacillo@outlook.com.br" className="email-me">
+            Entre em contato comigo por e-mail
+        </a>
+    );
+}
