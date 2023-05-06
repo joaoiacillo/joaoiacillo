6eda56292b13ac989e87202754a44068a44b5ee3
João Iacillo <joaopiacillo@outlook.com.br>
2023-05-05 22:26:19 -0300

    🔥 Remove React Files

--- src/components/EmailMe.js
+++
@@ -1,9 +0,0 @@
-import './EmailMe.css';
-
-export default function EmailMe() {
-    return (
-        <a href="mailto:joaopiacillo@outlook.com.br" className="email-me">
-            Entre em contato comigo por e-mail
-        </a>
-    );
-}
