d013962839838d36709ec37ee541644db5d8792f
João Iacillo <joaopiacillo@outlook.com.br>
2023-05-08 14:50:35 -0300

    ✨ Add Mine Projects

--- js/projects.js
+++ js/projects.js
@@ -1,63 +1,60 @@
-function createProjectElement(imgSrc, title, date) {
+const projectsElement = $("#projects ul");
+const projectsAmount = parseInt(projectsElement.data("amount"));
+
+const PROJECTS_IMGS_FOLDER = "imgs/projects/";
+
+function createProjectElement(imgSrc, title, link) {
     const imgElement = $("<img>").attr("src", imgSrc);
     const titleElement = $("<h3>").text(title);
-    const dateElement = $("<span>").addClass("generic").text(date);
+    const linkElement = $("<a>").attr("href", link).text("Acessar");
 
     const projectElement = $("<li>").append(
         imgElement,
         titleElement,
-        dateElement
+        linkElement
     );
 
     return projectElement;
 }
 
-const projectsElement = $("#projects ul");
-
-const projectsAmount = parseInt(projectsElement.data("amount"));
-
-const placeholderUrl = `https://placehold.co/1280x720`;
-
-function generateRandomProject() {
-    return createProjectElement(placeholderUrl, "Project Title", "01/01/2023");
+function extendImageUrl(filename) {
+    return PROJECTS_IMGS_FOLDER + filename;
 }
 
-for (let projectId = 0; projectId < projectsAmount; projectId++) {
-    const projectElement = generateRandomProject();
-    projectsElement.append(projectElement);
+const projectsJson = fetch("projects.json")
+    .then((response) => response.json())
+    .then((data) => handleProjectsJsonData(data))
+    .catch((err) => handleProjectsJsonError(err));
+
+function handleProjectsJsonData(data) {
+    for (let i = 0; i < projectsAmount; i++) {
+        const project = data[i];
+
+        if (typeof project === "undefined") break;
+
+        const { img, title, link } = project;
+
+        const projectElement = createProjectElement(
+            extendImageUrl(img),
+            title,
+            link
+        );
+
+        projectsElement.append(projectElement);
+    }
 }
 
-// const projectsJson = fetch("projects.json")
-//     .then((response) => response.json())
-//     .then((data) => handleProjectsJsonData(data))
-//     .catch((err) => handleProjectsJsonError(err));
+function handleProjectsJsonError(err) {
+    console.error(err);
 
-// function handleProjectsJsonData(data) {
-//     $("#open").click(function () {
-//         const id = this.dataset.id;
-//         if (!(id in data)) {
-//             return alert("Invalid id: " + id);
-//         }
+    const errMsg = $("<p>")
+        .css({
+            color: "red",
+            textAlign: "center",
+        })
+        .text(
+            "Something went wrong while loading the projects. Please check your browser console for more information."
+        );
 
-//         const project = data[id];
-
-//         $("#project img").attr("src", project.img);
-//         $("#project h3").text(project.title);
-//         $("#project a").text(project.link).attr("href", project.link);
-//     });
-// }
-
-// function handleProjectsJsonError(err) {
-//     console.error(err);
-
-//     const errMsg = $("<p>")
-//         .css({
-//             color: "red",
-//             textAlign: "center",
-//         })
-//         .text(
-//             "Something went wrong while loading the projects. Please check your browser console for more information."
-//         );
-
-//     $(projectsElement.parent()).append(errMsg);
-// }
+    $(projectsElement.parent()).append(errMsg);
+}
